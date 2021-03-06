af7249405e02bf9a4b9bae1cfa0773fa695b1fa0
Timothy Hobbs <timothy@hobbs.cz>
2021-03-06 19:05:30 +0100

    Set up automatic deployment with circleci

--- apply.sh
+++ apply.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-kubectl apply -f manifests/config-maps/
-kubectl apply -f manifests/ingress/
-kubectl apply -f manifests/
-ytt -f manifests/ytt/ -f  manifests/ytt/lib/ | kubectl apply -f -
+if [ ! -z "$KUBERNETES_CLUSTER_CERTIFICATE" ]
+then
+    echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+fi
+
+kubecmd (){
+    if [ -z "$KUBERNETES_TOKEN" ]
+    then
+        kubectl $@
+    else
+        kubectl \
+            --kubeconfig=/dev/null \
+            --server=$KUBERNETES_SERVER \
+            --certificate-authority=cert.crt \
+            --token=$KUBERNETES_TOKEN \
+            $@
+    fi
+}
+kubecmd apply -f manifests/config-maps/
+kubecmd apply -f manifests/ingress/
+kubecmd apply -f manifests/
+ytt -f manifests/ytt/ -f  manifests/ytt/lib/ | kubecmd apply -f -
