db064410f02ca74ae0f6b8a65b51646b438294cf
Timothy Hobbs <timothy@hobbs.cz>
2021-09-16 21:28:35 +0200

    capture_output isn't supported in python3.6 which is apparently what circleci uses

--- backups/build.py
+++ backups/build.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import subprocess
-result = subprocess.run("git diff --name-only HEAD~..HEAD | grep backups/", shell=True, capture_output=True)
+from subprocess import PIPE
+
+result = subprocess.run("git diff --name-only HEAD~..HEAD | grep backups/", shell=True, stdout=PIPE)
 if result.stdout:
     subprocess.run("docker build . -t auto0mat/k8s-backups:latest", shell=True)
     subprocess.run("docker login -u $DOCKER_USER -p $DOCKER_PASS", shell=True)
