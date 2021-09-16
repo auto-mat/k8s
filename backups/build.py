b0835e1c5ba2aaa258084360b6d31927ea4d48c8
Timothy Hobbs <timothy@hobbs.cz>
2021-09-16 22:46:29 +0200

    Tag backup image with build number

--- backups/build.py
+++ backups/build.py
@@ -6,8 +6,8 @@ result = subprocess.run("git diff --name-only HEAD~..HEAD | grep backups/", shel
 if result.stdout:
     import os
     os.chdir("./backups/")
-    subprocess.run("docker build . -t auto0mat/k8s-backups:latest", shell=True)
+    subprocess.run("docker build . -t auto0mat/k8s-backups:$CIRCLE_BUILD_NUM", shell=True)
     subprocess.run("docker login -u $DOCKER_USER -p $DOCKER_PASS", shell=True)
-    subprocess.run("docker push auto0mat/k8s-backups:latest", shell=True)
+    subprocess.run("docker push auto0mat/k8s-backups:$CIRCLE_BUILD_NUM", shell=True)
 else:
     print("Nothing to build, backup files not changed.")
