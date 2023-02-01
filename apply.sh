d8567e482597c2956f41b1d568965fd23bce0b24
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-02-01 18:47:46 +0100

    Use k8 token to deploy docker img to k8 cluster

--- apply.sh
+++ apply.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-if [ ! -z "$KUBERNETES_CLUSTER_CERTIFICATE" ]
-then
-    echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-fi
-
-kubecmd (){
-    if [ -z "$KUBERNETES_TOKEN" ]
-    then
-        kubectl $@
-    else
-        kubectl \
-            --kubeconfig=/dev/null \
-            --server=$KUBERNETES_SERVER \
-            --certificate-authority=cert.crt \
-            --token=$KUBERNETES_TOKEN \
-            $@
-    fi
-}
-kubecmd apply -f manifests/config-maps/
-kubecmd apply -f manifests/ingress/
-kubecmd apply -f manifests/
-ytt -f manifests/ytt/ -f manifests/ytt/lib/ -f manifests/ytt/config-maps/ -f manifests/ytt/data/ | kubecmd apply -f -
+kubectl apply -f manifests/config-maps/
+kubectl apply -f manifests/ingress/
+kubectl apply -f manifests/
+ytt -f manifests/ytt/ -f manifests/ytt/lib/ -f manifests/ytt/config-maps/ -f manifests/ytt/data/ | kubectl apply -f -
